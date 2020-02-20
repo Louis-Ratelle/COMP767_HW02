82bc0f995cf3dc9a5b08e337dc447f5176193a31
Louis-Francois Preville-Ratelle <lfprevilleratelle@gmail.com>
2020-02-20 11:44:45 -0500

    for mean square (without variance)

--- HW02Q01.py
+++ HW02Q01.py
@@ -22,6 +22,8 @@ SEED = None
 NB_EPISODES = 25 # 25
 NB_RUNS = 50 # 50
 
+seed_count = 1
+
 #set_LFPR = set()
 
 # #############################################################################
@@ -114,6 +116,19 @@ def plot4(title, training_return, training_regret, testing_reward, testing_regre
     axs[1, 1].set_title('Total testing regret')
 
 
+def plot_lambdas_w_alphas(d_msve, title = None):
+    for lammbda in d_msve.keys():
+        d_lammbda = d_msve[lammbda]
+        l_alphas_for_lammbda= []
+        for alpha in d_lammbda.keys():
+            l_alphas_for_lammbda.append(alpha)
+        l_alphas_for_lammbda.sort()
+        x = l_alphas_for_lammbda
+        y = [d_lammbda[alpha] for alpha in l_alphas_for_lammbda]
+        print("x: ", x)
+        print("y: ", y)
+
+
 # #############################################################################
 #
 # Helper functions
@@ -151,6 +166,7 @@ class tiling10():
         np.random.seed(seed)
         self.shift_10_tilings = self.create_tilings()
 
+
     def create_tilings(self):
         a = np.random.uniform(low=0.0, high=.1,size=10)
         a = np.sort(a)
@@ -170,19 +186,20 @@ class tiling10():
 
 
 class td_lambda_agent():
-    def __init__(self,alpha, lammbda, tiling, seeds):
+    def __init__(self,alpha, lammbda, tiling):
         self.alpha = alpha
         self.lammbda = lammbda
         self.ws = np.array([np.zeros(11*10) for _ in range(NB_RUNS)]) # is there a bias here, I assume no ??
         # In addition, add a feature corresponding to each interval that takes the value 1 when the state was within that tile, and 0 otherwise ??
         self.tiling = tiling
-        assert len(seeds) == NB_RUNS, "seed must have size NB_RUNS"
-        self.seeds = seeds
         self.average_w = np.zeros(11*10)
 
     def train_all_runs(self):
         for run_id in range(len(self.ws)):
-            np.random.seed(self.seeds[run_id])
+            global seed_count
+            np.random.seed(seed_count)
+            #print("seed_count: ", seed_count)
+            seed_count += 1
             self.train_one_run(run_id)
         self.average_w = np.mean(self.ws, axis=0)
         #return self.ws
@@ -253,45 +270,49 @@ def main():
 
     agents = {}
 
-    d_alpha_to_lammbdas = {}
-    #d_alpha_to_lammbdas[0.025] = np.round(np.arange(21) * 0.05, 2)
-    #d_alpha_to_lammbdas[0.05] = np.round(np.arange(20) * 0.05,2)
-    #d_alpha_to_lammbdas[0.075] = np.round(np.arange(19) * 0.05, 2)
-    #d_alpha_to_lammbdas[0.1] = np.round(np.arange(17) * 0.05, 2)
-    #d_alpha_to_lammbdas[0.125] = np.round(np.arange(14) * 0.05, 2)
-    #d_alpha_to_lammbdas[0.14] = np.round(np.arange(14) * 0.05, 2)
-    d_alpha_to_lammbdas[0.50] = np.round(np.arange(14) * 0.05, 2)
 
+    d_lammbda_to_alphas = {}
 
+    d_lammbda_to_alphas[0.0] = np.round(np.arange(15) * 0.01, 2)
+    d_lammbda_to_alphas[0.1] = np.round(np.arange(15) * 0.01, 2)
+    d_lammbda_to_alphas[0.2] = np.round(np.arange(16) * 0.01, 2)
+    #d_lammbda_to_alphas[0.3] = np.round(np.arange(16) * 0.01, 2)
+    #d_lammbda_to_alphas[0.4] = np.round(np.arange(15) * 0.01, 2)
+    #d_lammbda_to_alphas[0.5] = np.round(np.arange(15) * 0.01, 2)
+    #d_lammbda_to_alphas[0.6] = np.round(np.arange(14) * 0.01, 2)
+    #d_lammbda_to_alphas[0.7] = np.round(np.arange(13) * 0.01, 2)
+    #d_lammbda_to_alphas[0.8] = np.round(np.arange(11) * 0.01, 2)
+    #d_lammbda_to_alphas[0.9] = np.round(np.arange(8) * 0.01, 2)
+    #d_lammbda_to_alphas[0.95] = np.round(np.arange(6) * 0.01, 2)
+    #d_lammbda_to_alphas[1.0] = np.round(np.arange(4) * 0.01, 2)
 
 
     d_msve = {}
 
     #for (alpha, lammbda) in [(0.05, 0.7)]: #[(0.03* i, 0.2) for i in range(0,10)]:
-    for alpha, l_lammbda in d_alpha_to_lammbdas.items():
-        d_msve[alpha] = {}
-        for lammbda in l_lammbda:
-            print("for alpha = {} and lammbda = {}".format(alpha, lammbda))
+    for lammbda, l_alphas in d_lammbda_to_alphas.items():
+        d_msve[lammbda] = {}
+        for alpha in l_alphas:
+            print("for lammbda = {} and alpha = {}".format(lammbda, alpha))
             #alpha = 0.1
             #lammbda = 0.5
-            #seeds = list(range(NB_RUNS))
-            seeds = np.arange(NB_RUNS)
-            #seeds = [6]
-            agents[(alpha, lammbda)]= td_lambda_agent(alpha, lammbda, tilings, seeds)
+            agents[(alpha, lammbda)]= td_lambda_agent(alpha, lammbda, tilings)
             agents[(alpha, lammbda)].train_all_runs()
             #print("ws: ", agents[(alpha, lammbda)].ws)
             #print("average w: :", agents[(alpha, lammbda)].average_w)
 
             valid_nums = np.round(np.arange(21) * 0.05,2)
             mean_square_error = agents[(alpha, lammbda)].msve(valid_nums)
-            print("msve for alpha = {} and lammbda = {}: ".format(alpha, lammbda), mean_square_error)
-            d_msve[alpha][lammbda] = mean_square_error
+            print("msve for lammbda = {} and alpha = {}: ".format(lammbda, alpha), mean_square_error)
+            d_msve[lammbda][alpha] = mean_square_error
 
 
     #print("set: ", set_LFPR)
     #print(len(set_LFPR))
 
-    print(d_msve)
+    #print(d_msve)
+
+    plot_lambdas_w_alphas(d_msve)
 
 
 if __name__ == '__main__':
